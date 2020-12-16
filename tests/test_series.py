4c87d04dacd825075831f378b9c934ec1378f9ea
Dario Thornhill <dario.thornhill@gmail.com>
2020-12-16 15:47:45 +0100

    Added sum_series tests

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,4 @@
-from math_series.series import fibonacci, lucas
+from math_series.series import fibonacci, lucas, sum_series
 
 def test_fibonacci():
     n = 4
@@ -10,4 +10,16 @@ def test_lucas():
     n = 4
     expected = 4
     actual = lucas(n)
+    assert(actual == expected)
+
+def test_sum_series_lucas():
+    n = 4
+    expected = 4
+    actual = sum_series(n, 2, 1)
+    assert(actual == expected)
+
+def test_sum_series_fib():
+    n = 4
+    expected = 3
+    actual = sum_series(n)
     assert(actual == expected)
