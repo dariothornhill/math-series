10ec1b81f40b8b978850649ebf81cba15c78506e
Dario Thornhill <dario.thornhill@gmail.com>
2020-12-16 15:38:32 +0100

    Added fibonacci

--- math_series/series.py
+++ math_series/series.py
@@ -1,13 +1,11 @@
 def fibonacci(n: int) -> int:
     ''' Calculates and returns the nth number fo the fibonacci series'''
-    if n == 1 or n ==2:
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
-    if n == 3:
-        return 2
-    if n == 4:
-        return 3
-    if n == 5:
-        return 5
+    return fibonacci(n-1) + fibonacci(n-2)
+    
     return 0
 
 def lucas(n: int) -> int:
