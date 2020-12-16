d8c1d649462170970a76928f5c1159af319ab530
Dario Thornhill <dario.thornhill@gmail.com>
2020-12-16 15:44:28 +0100

    Added lucas series function

--- math_series/series.py
+++ math_series/series.py
@@ -5,12 +5,16 @@ def fibonacci(n: int) -> int:
     if n == 1:
         return 1
     return fibonacci(n-1) + fibonacci(n-2)
-    
-    return 0
 
 def lucas(n: int) -> int:
     ''' Calculates and returns the nth number fo the lucas series'''
-    return 0
+    if n == 0:
+        return 0
+    if n == 1:
+        return 2
+    if n == 2:
+        return 1
+    return lucas(n-1) + lucas(n-2)
 
 def sum_series(n: int, a = 0, b = 1 ) -> int:
     ''' Calculates and returns the nth number of various summation series series'''
