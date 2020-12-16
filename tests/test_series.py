d1566df822b4316801b9c602e54678628bb311bd
Dario Thornhill <dario.thornhill@gmail.com>
2020-12-16 15:41:05 +0100

    Added test for lucas series

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,4 +1,4 @@
-from math_series.series import fibonacci
+from math_series.series import fibonacci, lucas
 
 def test_fibonacci():
     n = 4
@@ -6,3 +6,8 @@ def test_fibonacci():
     actual = fibonacci(n)
     assert(actual == expected)
 
+def test_lucas():
+    n = 4
+    expected = 4
+    actual = lucas(n)
+    assert(actual == expected)
